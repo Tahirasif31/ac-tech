743c929aded099c6e6c83dd21043c14e778bb178
tahirasif31 <tahirasif31@gmail.com>
2024-03-09 16:11:30 +0500

    experts section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Hero } from "./../components/Hero";
-import { About } from "./../components/about";
+import { About } from "./../components/About";
+import { ExpertsTeam } from "@/components/ExpertsTeam";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <ExpertsTeam />
     </div>
   );
 }
