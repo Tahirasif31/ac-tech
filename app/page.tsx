25c950834da9c9950a0b0919d10ffee9c5917afb
tahirasif31 <tahirasif31@gmail.com>
2024-03-09 18:27:31 +0500

    hero button fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Hero } from "./../components/Hero";
 import { About } from "./../components/About";
 import { ExpertsTeam } from "@/components/ExpertsTeam";
+import { ThreeDCard } from "@/components/ThreeDCard";
+import { LampSection } from "@/components/LampSection";
 
 export default function Home() {
   return (
--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "./ui/moving-borders";
 import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 export function HeroText() {
   const words = [
@@ -25,17 +26,20 @@ export function HeroText() {
   ];
   return (
     <div className="flex flex-col items-center justify-center h-[40rem] z-50 ">
-      <p className="text-neutral-200 text-xs sm:text-base  ">
+      <p className="text-neutral-200 text-[13px] sm:text-base  ">
         Efficient solutions for your AC troubles.
       </p>
       <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-          Join now
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="animate-shimmer py-1 text-[13px] md:py-[9px] bg-slate-900 items-center justify-center rounded-[1.75rem] border-neutral-200  border  bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          Whatsapp
         </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
-        </button>
+        <Button
+          borderRadius="1.75rem"
+          className="dark:bg-slate-900 text-[13px] text-white dark:text-white border-neutral-200 dark:border-slate-800 px-1 py-[12px]"
+        >
+          Book Now
+        </Button>
       </div>
     </div>
   );
