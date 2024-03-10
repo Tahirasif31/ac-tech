409ce2559f26344288c3d3d888c82f179eebeb54
tahirasif31 <tahirasif31@gmail.com>
2024-03-10 15:32:04 +0500

    navbar responsive completed

--- components/Nav.js
+++ components/Nav.js
@@ -1,73 +1,52 @@
 "use client";
 import { Button } from "./ui/moving-borders";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
-import { cn } from "@/utils/cn";
-
 function Nav() {
-  const [active, setActive] = useState(null);
+  const [showNav, setShowNav] = useState();
   return (
-    <nav className="flex justify-between  mt-4 z-50 align-middle items-center 2xl:mx-28 xl:mx-20 md:mx-8 border border-[#FFFFFF]/[0.16] px-4 rounded-lg">
+    <nav className="flex justify-between mt-4 z-50 align-middle items-center 2xl:mx-28 xl:mx-20 md:mx-8 border border-[#FFFFFF]/[0.16] px-4 rounded-lg bg-[#00000016]">
       <span>Ac Tech</span>
-
-      <div className={cn("max-w-2xl mx-auto z-50")}>
-        <Menu setActive={setActive} className="text-white">
-          <MenuItem setActive={setActive} active={active} item="Services">
-            <div className="flex flex-col space-y-4 text-sm text-white">
-              <HoveredLink href="/web-dev" className="text-white">
-                Web Development
-              </HoveredLink>
-              <HoveredLink href="/interface-design">
-                Interface Design
-              </HoveredLink>
-              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-              <HoveredLink href="/branding">Branding</HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
-            <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-              <ProductItem
-                title="Algochurn"
-                href="https://algochurn.com"
-                src="https://assets.aceternity.com/demos/algochurn.webp"
-                description="Prepare for tech interviews like never before."
-              />
-              <ProductItem
-                title="Tailwind Master Kit"
-                href="https://tailwindmasterkit.com"
-                src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-                description="Production ready Tailwind css components for your next project"
-              />
-              <ProductItem
-                title="Moonbeam"
-                href="https://gomoonbeam.com"
-                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-                description="Never write from scratch again. Go from idea to blog in minutes."
-              />
-              <ProductItem
-                title="Rogue"
-                href="https://userogue.com"
-                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-                description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-              />
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Pricing">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="/hobby">Hobby</HoveredLink>
-              <HoveredLink href="/individual">Individual</HoveredLink>
-              <HoveredLink href="/team">Team</HoveredLink>
-              <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-            </div>
-          </MenuItem>
-        </Menu>
-      </div>
-      <Button
-        borderRadius="1.75rem"
-        className="dark:bg-slate-900 text-white dark:text-white border-neutral-200 dark:border-slate-800 px-1 py-3"
+      <div
+        className={`md:static absolute bg-[#0000004f] md:bg-transparent min-h-[16vh] left-0 ${
+          showNav ? "top-[8%]" : "top-[-100%]"
+        } py-3 md:min-h-fit w-full md:w-auto flex items-center md:px-5`}
       >
-        Login
-      </Button>
+        <ul className="flex md:flex-row flex-col md:gap-[4vw] px-4 gap-8 md:items-center">
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>Team</li>
+        </ul>
+      </div>
+      <div className="flex gap-2">
+        <Button
+          borderRadius="1.75rem"
+          className="dark:bg-slate-900 text-white dark:text-white border-neutral-200 dark:border-slate-800 px-1 py-3 "
+        >
+          Login
+        </Button>
+        <button
+          type="button"
+          onClick={() => setShowNav(!showNav)}
+          className="md:hidden cursor-pointer"
+        >
+          <div
+            className={`w-[23px] h-[2px] transition ease-in duration-150  bg-white ${
+              showNav ? "rotate-45 mb-0  translate-y-[1px]" : "rotate-0 mb-1"
+            } `}
+          ></div>
+          <div
+            className={`w-[23px] h-[2px] transition ease-in duration-150 bg-white ${
+              showNav ? "hidden mb-0" : "mb-1"
+            } `}
+          ></div>
+          <div
+            className={`w-[23px] h-[2px] transition ease-in duration-150 bg-white  ${
+              showNav ? "-rotate-45 mb-0" : "rotate-0"
+            }`}
+          ></div>
+        </button>
+      </div>
     </nav>
   );
 }
