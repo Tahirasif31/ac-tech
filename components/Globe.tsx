8079dfe7f0ceec94d1c08a5d60e91976b99dabd2
tahirasif31 <tahirasif31@gmail.com>
2024-03-14 15:41:29 +0500

    another fixed

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -427,7 +427,7 @@ export function GlobeDemo() {
           </p>
         </motion.div>
         {/* <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent  z-40" /> */}
-        <div className="absolute w-full bottom-20 md:-bottom-2  h-72 md:h-full z-10">
+        <div className="absolute w-full bottom-20 md:-bottom-20  h-72 md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
