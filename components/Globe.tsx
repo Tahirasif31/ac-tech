4d07b8e9a7ac0a7907a727e72557724d4f607cba
tahirasif31 <tahirasif31@gmail.com>
2024-03-14 15:35:12 +0500

    bug fixed

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -415,14 +415,15 @@ export function GlobeDemo() {
             Book Your Service Now!
           </h2>
           <p className="text-center text-base md:text-lg font-normal text-neutral-200 max-w-md mt-2 mx-auto mb-20 z-50">
-            Get your AC fixed or get a quote fast! Chat with our experts on{" "}
+            Get your AC fixed or get a quote fast! Chat with our experts
+            on&nbsp;
             <a
               href="#"
               className="bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text cursor-pointer z-50"
             >
               Whatsapp
-            </a>{" "}
-            now!"{" "}
+            </a>
+            &nbsp;now!"
           </p>
         </motion.div>
         {/* <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent  z-40" /> */}
